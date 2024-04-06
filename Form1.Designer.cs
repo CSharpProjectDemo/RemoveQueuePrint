a6479defe6dd8b2120fd3266ab3595e572c00622
Huy Torres <huytorres10@gmail.com>
2024-04-06 22:02:07 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace RemoveQueuePrint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnRemoveQueue = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRemoveQueue
+            // 
+            this.btnRemoveQueue.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRemoveQueue.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnRemoveQueue.FlatAppearance.BorderSize = 0;
+            this.btnRemoveQueue.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRemoveQueue.ForeColor = System.Drawing.Color.Red;
+            this.btnRemoveQueue.Location = new System.Drawing.Point(6, 9);
+            this.btnRemoveQueue.Name = "btnRemoveQueue";
+            this.btnRemoveQueue.Size = new System.Drawing.Size(273, 38);
+            this.btnRemoveQueue.TabIndex = 1;
+            this.btnRemoveQueue.Text = "Xoá hàng đợi máy in";
+            this.btnRemoveQueue.UseVisualStyleBackColor = false;
+            this.btnRemoveQueue.Click += new System.EventHandler(this.btnRemoveQueue_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(284, 57);
+            this.Controls.Add(this.btnRemoveQueue);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Xoá hàng chờ máy in";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnRemoveQueue;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Printing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RemoveQueuePrint
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btnRemoveQueue_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Tạo một query để lấy danh sách các hàng đợi in
+                SelectQuery query = new SelectQuery("SELECT * FROM Win32_PrintJob");
+
+                // Tạo một searcher để thực thi query
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
+
+                // Lặp qua danh sách các công việc in và xóa chúng
+                foreach (ManagementObject job in searcher.Get())
+                {
+                    job.Delete();
+                }
+
+                // Hiển thị thông báo khi xóa thành công
+                MessageBox.Show("Đã xóa hàng đợi in thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                // Hiển thị thông báo nếu có lỗi xảy ra
+                MessageBox.Show("Đã xảy ra lỗi: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
